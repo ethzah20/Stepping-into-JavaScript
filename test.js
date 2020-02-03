9d206b57c33080fbfefc2cd98386786fc6d78e21
ethzah20 <60369654+ethzah20@users.noreply.github.com>
2020-02-03 18:35:49 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-var youLikeMeat = true;
-var myNum = 10;
+var myAge = 29;
 
-if (youLikeMeat){
-  document.write("you like meat");
+if (myAge > 30){
+  document.write("You are over 30!");
+} else if (myAge > 20){
+  document.write("You are over 20!");
+} else if (myAge > 10){
+  document.write("You are over 10!");
 } else{
-  document.write("you hate meat");
+  document.write("You are not over 10!");
 }
 
