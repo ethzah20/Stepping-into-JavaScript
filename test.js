c40dffd8092f36cf2efa5ec9500aa8ee142591cf
ethzah20 <60369654+ethzah20@users.noreply.github.com>
2020-02-10 17:44:26 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-for (i = 0; i< 10; i++){
-  if(i === 5 || i === 3){
-    continue;
-  }
-  console.log(i);
-  if(i ===7){
-    break;
-  }
-}
-console.log("I have broken out of the loop");
+var links = document.getElementsByTagName("a");
+
+for (i = 0; i<= links.length; i++){
   
+  links[i].ClassName = "link-" + i;
+}
 
 
