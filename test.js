e0e53319191f047587c02792a2a76be0e8780c50
ethzah20 <60369654+ethzah20@users.noreply.github.com>
2020-02-10 18:39:05 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-function getAverage (a,b,c,d,e,f){
-  var average = (a+b+c+d+e+f)/6;
-  console.log (avaerage);
+var average = 0;
+function getAverage (a,b){
+  average = (a+b) / 2;
+  console.log (average);
   return average;
 }
-var myResult = getAverage(7,8,9,10,11,12);
-console.log("the average is" + myResult);
-
+var myResult = myAverage(7,11); // global variable
+function LogResult(){
+  console.log("the average is" + myResult +"inside the function");
+}
+LogResult();
 
